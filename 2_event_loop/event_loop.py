ebad059a5e7b952ef5229382fd3c65c02031833a
shaydulin <m.shaydulin@yandex.ru>
2024-08-09 15:11:05 +0400

    learn first lesson

---
+++ 2_event_loop/event_loop.py
@@ -0,0 +1,45 @@
+import socket
+from select import select
+
+TO_MONITOR = []
+CONNECTIONS = {}
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(("127.0.0.1", 5000))
+server_socket.listen()
+TO_MONITOR.append(server_socket)
+
+
+def accept_connection(server_socket):
+    client_socket, addr = server_socket.accept()
+    print("Connection from: ", addr)
+    TO_MONITOR.append(client_socket)
+    CONNECTIONS[client_socket] = addr
+
+
+def send_message(client_socket: socket.socket):
+    request = client_socket.recv(4096)
+    if request:
+        response = b"Hello world\n"
+        client_socket.send(response)
+    else:
+        client_socket.close()
+        TO_MONITOR.remove(client_socket)
+        print(f"Connection with {CONNECTIONS[client_socket]} closed.")
+        del CONNECTIONS[client_socket]
+
+
+def event_loop():
+    while True:
+        ready_to_read, _, _ = select(TO_MONITOR, [], [])
+
+        for sock in ready_to_read:
+            if sock is server_socket:
+                accept_connection(sock)
+            else:
+                send_message(sock)
+
+
+if __name__ == "__main__":
+    event_loop()
--- README.md
+++ README.md
@@ -6,3 +6,13 @@
 - run nc 127.0.0.1 5000 in second terminal
 - run nc 127.0.0.1 5000 in third terminal
 - see: write something in second and third terminals, server responds in second but no response in third terminal
+
+### Event loop.
+
+Program 2_event_loop/event_loop.py uses select() function to monitor list of files (sockets in this case) that are ready to read (but also to write and list of files with errors).
+
+When some is readable select() return list of them. And we can read that sockets.
+
+- run 2_event_loop/event_loop.py
+- run nc 127.0.0.1 5000 in as many terminals as You want
+- see: write something in any terminal, server responds instantly
